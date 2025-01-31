39434f8046db8551ae633d65c2b942dc42f33321
Alec Mirambeau <alecmirambeau@gmail.com>
2025-01-31 14:59:19 -0500

    solved more optimally

---
+++ 15_3Sum/twoPointerApproach.py
@@ -0,0 +1,45 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 3:
+            return [nums] if sum(nums) == 0 else []
+        # list is bigger than 3 items
+        sumsFound = set()
+        nums.sort()
+        for i in range(0, len(nums)):
+            l = i + 1
+            r = len(nums) - 1
+            prevl = ''
+            prevr = ''
+            while l < r:
+                if prevl == nums[l]:
+                    l += 1
+                    continue
+                elif prevr == nums[r]:
+                    r -= 1
+                    continue
+                numsSum = nums[i] + nums[l] + nums[r]
+                if numsSum == 0:
+                    sumsFound.add((nums[i], nums[l], nums[r]))
+                    prevl = nums[l]
+                    prevr = nums[r]
+                    l += 1
+                    r -= 1
+                elif numsSum > 0:
+                    prevr = nums[r]
+                    r -= 1
+                elif numsSum < 0:
+                    prevl = nums[l]
+                    l += 1
+        return [list(sol) for sol in sumsFound]
+
+"""
+nums = [-1,0,1,2,-1,-4]
+numsSorted = [-4, -1, -1, 0, 1, 2]
+nums[i] = -1
+nums[l] = 0
+nums[r] = 1
+prevl = -1
+prevr = 2
+numsSum = 
+sumsFound = {(-1, -1, 2), (-1, 0, 1)}
+"""
