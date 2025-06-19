e6d637921d897a0eff49c0f46c623a1ee6c5bcfa
Alec Mirambeau <alecmirambeau@gmail.com>
2025-06-19 12:55:48 -0400

    updated to do optimal sliding window solution

--- 3LongestSubstringWithoutRepeatingCharacters/MoreReadableSlidingWindow.py
+++ 3LongestSubstringWithoutRepeatingCharacters/MoreReadableSlidingWindow.py
@@ -1,22 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        longest_sub = 0
-        sub = set()
         l = 0
-        for r in range(0, len(s)):
-            if s[r] in sub:
-                while s[r] in sub:
-                    sub.remove(s[l])
-                    l += 1
-            sub.add(s[r])
-            longest_sub = max(longest_sub, len(sub))
-        return longest_sub
-
-"""
-s = "dvdf"
-longest = 2
-sub = {v, d, f}
-l = 1
-r = 3
-
-"""
+        ls = 0
+        seen = {}
+        for r in range(len(s)):
+            new_let = s[r]
+            if new_let in seen and seen[new_let] >= l:
+                l = seen[new_let] + 1
+            seen[new_let] = r
+            ls = max(ls, r - l + 1)
+        return ls
