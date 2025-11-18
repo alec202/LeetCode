0980dc368bf7f70982a2eae968269e11d04de1ca
Alec Mirambeau <alecmirambeau@gmail.com>
2025-11-18 15:24:02 -0500

    where at in retry

--- 76MinimumWindowSubstring/unoptimizedSolution.py
+++ 76MinimumWindowSubstring/unoptimizedSolution.py
@@ -1,55 +1,58 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # turn t to dictionary for O(1) lookup & keeping track of number of times we see
-        # each character
-
-        # minSeen = -1
-        # for l in range(len(s))
-        # reset dictionary for keeping track of the character count
-        # for r in
-        from collections import Counter
-        cDict = Counter(t)
-        tCount = dict(cDict)
-        minSeen = -1
-        minString = ""
+        """
+        - make winDictionary to keep track of the number of t characters we have in our window X
+        - make t dictionary to track number of each character in t X
+        - have = 0 to track correct # of characters we have in our window
+        - need = len(t)
+        - nested loop outer loop is l, inner loop is r.
+        -X if this char == tDict[char]
+            if true, update have += 1
+        - if have == need:
+            - if false then we keep expanding our window until we hit the end
+            - if true then we have a potential new min substring
+                - if true, update min sub string, and update tracked indices for min substring
+                    - then break from inner loop to cause outer loop to shift forward a character
+                    -X check if this char that outer loop, l, points to is in our winDict, if not we skip this iteration using continue statement, and if it is in our winDict we will continue executing the code within this iteration.
+                    -X inside inner loop reset the dictionary values to be 0
+        - once end of input string s, slice s and return that slice.
+        """
+        winDict, tDict = {}, {}
+        for char in t:
+            winDict[char] = 0
+            tDict[char] = tDict.get(char, 0) + 1
+        # built our winDict and tDict
+        have, need = 0, len(t)
+        minSub = [-1, -1]
+        minSeen = float("inf")
         for l in range(len(s)):
-            windowCount = {}
+            outerChar = s[l]
+            if outerChar not in tDict:
+                continue
             for r in range(l, len(s)):
-                currChar = s[r]
-                windowCount[currChar] = windowCount.get(currChar, 0) + 1
-                if currChar in tCount and tCount[currChar] <= windowCount[currChar]:
-                    # do if statement to check if allPresent is True.
-                    # if allPresent is True then we will break out of the inner loop and update
-                    # the min window substring
-                    if self.is_solution(windowCount, tCount):
-                        break
-                # if allPresent is false, then we just keep expanding our window like normal
-
-            if self.is_solution(windowCount, tCount):
-                if minSeen == -1:
-                    minSeen = r - l + 1
-                    minString = s[l:r + 1]
-                else:
-                    minSeen = min(minSeen, (r - l + 1))
-                    if minSeen == (r - l + 1):
-                        minString = s[l:r + 1]
-        return minString
-
-    def is_solution(self, windowCount, tCount):
-        for char in tCount:
-            if tCount[char] > windowCount.get(char, -1):
-                # if not correct amount in windowCount then break from check
-                return False
-        return True
+                char = s[r]
+                winDict[char] = 1 + winDict.get(char, 0)
+                if char in tDict:
+                    if tDict[char] == winDict[char]:
+                        have += 1
+                        if have == need:
+                            winSize = r - l + 1
+                            minSeen = min(winSize, minSeen)
+                            if minSeen == winSize:
+                                minSub = [l, r]
+                            winDict = {}
+                            for char in tDict:
+                                winDict[char] = 0
+                            break
+        return s[minSub[0]: minSub[1] + 1] if minSeen != float("inf") else ""
 
 
 """
-                for optimized version,
-                we do sliding window, but rather than checking every character like done here, we'd start from the first character that we see in our s string that is in our t string. then expand our window from the right side until we get all characters from t with the correct amount, then once we do this, we update our minimum window seen so far. Then we will need to shift our left pointer:
-                we will do this by first removing from our window dictionary what the left pointer is currently pointing to, then once it's removed from our windowDictionary, we shift our left pointer right by 1 character:
-            l += 1 
-            we will keep on removing from our window dictionary and shifting over out left pointer until we get to the next character in the string s that is also in t. Then we start expanding our window from here until we get all of the characters in t. Then we update min again, and resize our window again, and keep repeating. 
 
-    If we hit the end of the string before we get all the characters in t (with the correct amounts), then we will return the empty string like asked. We could do a check on if our min seen window = -1 since -1 wouldn't make sense in this context and we know that we'd never update the minSeen variable to be -1. If we end the loop and minSeen = -1, we return ""
 """
 
+sol = Solution()
+s = "ADOBECODEBANC"
+t = "ABC"
+x = sol.minWindow(s, t)
+print(x)
--- test.py
+++ test.py
@@ -1,22 +1,3 @@
-def lengthOfLongestSubstring(s: str) -> int:
-    ls = 0
-    left = 0
-    right = left + 1
-    if len(s) < 2:
-        return len(s)
-
-    let_indx = {s[0]: 0}
-    while right < len(s):
-        new_lett = s[right]
-        if new_lett in let_indx:
-            new_l_index = let_indx[new_lett] + 1
-            while left != new_l_index:
-                let_indx.pop(s[left], "")
-                left += 1
-            let_indx[new_lett] = left
-        else:
-            let_indx[new_lett] = right
-        right += 1
-        ls = max(ls, len(let_indx))
-    return ls
-print(lengthOfLongestSubstring("aabaab!bb"))
+s = {"a":1, 2:2}
+b = {"a":1, 2:2}
+print(s == b)
